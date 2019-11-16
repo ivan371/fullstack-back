8e8539c5a456b70c881bb1afdff32a5110879c49
Ivan Nagaiko <i.nagaiko@corp.mail.ru>
2019-11-16 12:05:46 +0300

    add old configuration

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +25,7 @@ SECRET_KEY = 'dirr3(ub4&xbcjpfs(i$=&xsj@+nlb^ed(wba83)-k8ui+)1%x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -141,5 +140,3 @@ CORS_ORIGIN_WHITELIST = 'http://localhost:3001',
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media/')
-
-django_heroku.settings(locals())
